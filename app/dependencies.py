3a795d1d615ca9d0367c0747f7bb0658285db31b
kytxii <tilleykyle12@gmail.com>
2026-03-29 00:14:16 -0700

    feat(dependencies): add async get_db

---
+++ app/dependencies.py
@@ -0,0 +1,5 @@
+from sqlalchemy.ext.asyncio import AsyncSession                                from app.database import AsyncSessionLocal                                   
+                                                                            
+async def get_db():
+    async with AsyncSessionLocal() as session:                               
+        yield session
