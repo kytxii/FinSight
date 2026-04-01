393747e3e1dcea53a409a27e2560ab31ac60402b
kytxii <tilleykyle12@gmail.com>
2026-03-31 23:47:59 -0700

    fix(middleware): remove allow_credentials from CORS settings

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ app = FastAPI(redirect_slashes=False)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
