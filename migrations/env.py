cdfd2bfbf0a0e63c4b42af5ef00775b0e3e8b82d
kytxii <tilleykyle12@gmail.com>
2026-04-01 01:15:42 -0700

    feat(migrations): set SQLAlchemy URL from settings in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -6,10 +6,12 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from app.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
