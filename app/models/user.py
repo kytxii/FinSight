c4d28ff2dabb3ea14748629d8fcf6295118e25eb
kytxii <tilleykyle12@gmail.com>
2026-04-01 14:14:25 -0700

    feat(users): add user model and schema

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String, DateTime, UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
+from app.database import Base
+import uuid
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    email_address: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(95), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
