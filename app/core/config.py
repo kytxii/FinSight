2bbeaf6744ce3b36dc24bbc374079f049c1cef70
kytxii <tilleykyle12@gmail.com>
2026-04-05 01:14:31 -0700

    fix(config): update access token expiration

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     FRONTEND_URL: str
     DEV_URL: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080
 
     model_config = {"env_file": ".env"}
 
